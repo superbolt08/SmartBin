f489a660f52176154f4038363dd66eaf3e42cd14
Saad <saadtab01@gmail.com>
2025-02-20 19:27:28 -0800

    set up

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,22 @@ const int echoPin = 18;
 
 // Bin full threshold (adjustable)
 const int binFullDistance = 5; // cm 
+
+void setup() {
+  Serial.begin(115200);
+
+  // WiFi Connection
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi!");
+
+  // Initialize Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  // Ultrasonic sensor setup
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
