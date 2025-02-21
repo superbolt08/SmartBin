a2307b039311bd51d0e394a068f2c349c91560d8
Saad <saadtab01@gmail.com>
2025-02-20 19:26:38 -0800

    declare variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
 #include <WiFi.h>
 #include <FirebaseESP32.h>
 #include "secrets.h" // Firebase credentials
+#include "config.h" // Wifi credentials
+
+// Firebase instance
+FirebaseData firebaseData;
+
+// Ultrasonic sensor pins
+const int trigPin = 5;
+const int echoPin = 18;
+
+// Bin full threshold (adjustable)
+const int binFullDistance = 5; // cm 
